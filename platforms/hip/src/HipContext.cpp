3002b36e40f3573dc16126984bef0dd7afc51b4d
Anton Gorenko <anton@streamhpc.com>
2024-08-25 20:20:50 +0500

    Port changes from the main repository
    CustomCPPForceImpl for writing forces in C++
    
        https://github.com/openmm/openmm/commit/9a0db72
        https://github.com/openmm/openmm/pull/4231
    
    Virtual sites can depend on other virtual sites
    
        https://github.com/openmm/openmm/commit/71f4b3f
    
    Use LF-Middle for LangevinIntegrator and VariableLangevinIntegrator
    
        https://github.com/openmm/openmm/commit/86988b9
    
    Merged more code into common platform
    
        https://github.com/openmm/openmm/commit/5739788
    
        * Common implementation of BondedUtilities
        * Common implementation of UpdateStateDataKernel
    
    Fixed periodic box changing from rectangular to triclinic
    
        https://github.com/openmm/openmm/commit/75d4f29

--- platforms/hip/src/HipContext.cpp
+++ platforms/hip/src/HipContext.cpp
@@ -6,7 +6,7 @@
  * Biological Structures at Stanford, funded under the NIH Roadmap for        *
  * Medical Research, grant U54 GM072970. See https://simtk.org.               *
  *                                                                            *
- * Portions copyright (c) 2009-2023 Stanford University and the Authors.      *
+ * Portions copyright (c) 2009-2024 Stanford University and the Authors.      *
  * Portions copyright (C) 2020-2023 Advanced Micro Devices, Inc. All Rights   *
  * Reserved.                                                                  *
  * Authors: Peter Eastman, Nicholas Curtis                                    *
@@ -46,6 +46,7 @@
 #include "openmm/common/ComputeArray.h"
 #include "openmm/common/ContextSelector.h"
 #include "SHA1.h"
+#include "openmm/MonteCarloFlexibleBarostat.h"
 #include "openmm/Platform.h"
 #include "openmm/System.h"
 #include "openmm/VirtualSite.h"
@@ -290,6 +291,9 @@ HipContext::HipContext(const System& system, int deviceIndex, bool useBlockingSy
     boxIsTriclinic = (boxVectors[0][1] != 0.0 || boxVectors[0][2] != 0.0 ||
                       boxVectors[1][0] != 0.0 || boxVectors[1][2] != 0.0 ||
                       boxVectors[2][0] != 0.0 || boxVectors[2][1] != 0.0);
+    for (int i = 0; i < system.getNumForces(); i++)
+        if (dynamic_cast<const MonteCarloFlexibleBarostat*>(&system.getForce(i)) != NULL)
+            boxIsTriclinic = true;
     if (boxIsTriclinic) {
         compilationDefines["APPLY_PERIODIC_TO_DELTA(delta)"] =
             "{"
@@ -669,6 +673,13 @@ hipFunction_t HipContext::getKernel(hipModule_t& module, const string& name) {
         throw OpenMMException(m.str());
     }
     return function;
+}
+
+vector<ComputeContext*> HipContext::getAllContexts() {
+    vector<ComputeContext*> result;
+    for (HipContext* c : platformData.contexts)
+        result.push_back(c);
+    return result;
 }
 
 hipStream_t HipContext::getCurrentStream() {
